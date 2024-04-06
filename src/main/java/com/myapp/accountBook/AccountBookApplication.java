174799286f1ca9800f35a097bb69c646fd765dad
Jin Lee <isabellalee82@gmail.com>
2024-04-06 11:24:03 +0900

    first commit

---
+++ src/main/java/com/myapp/accountBook/AccountBookApplication.java
@@ -0,0 +1,13 @@
+package com.myapp.accountBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myapp/accountBook/AccountBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myapp.accountBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
